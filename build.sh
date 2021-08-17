78d487f347c5519b1d52199cecbd5f2e6490f19c
Nick Brell <nickbrell@yahoo.com>
2021-08-17 14:44:09 -0500

    modified build.sh for jenkins job

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "printenv"
-echo "==========================="
-printenv
+#echo "printenv"
+#echo "==========================="
+#printenv
 
-echo "echo PATH"
-echo "=========================="
-echo $PATH
+#echo "echo PATH"
+#echo "=========================="
+#echo $PATH
 
 
 #docker-compose -f deploy/docker-compose.yml run --rm terraform plan
