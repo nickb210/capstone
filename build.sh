57fd7828f9c0fbe753aeedec69daea39ba9aab46
Nick Brell <nickbrell@yahoo.com>
2021-08-18 09:16:56 -0500

    cleaned up file

--- build.sh
+++ build.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-echo "printenv"
-echo "==========================="
-printenv
-
-#echo "echo PATH"
-#echo "=========================="
-#echo $PATH
-
-ls
-
-echo "\naws-vault list"
-echo "********************************************************************"
-aws-vault list
-
-echo "\naws-vault exec nick.brell -d 12h"
-echo "********************************************************************"
-#aws-vault exec nick.brell -d 12h
-#aws-vault exec nick.brell
-#echo $AWS_VAULT
-
 
 echo "\ndocker-compose -f deploy/docker-compose.yml run --rm terraform init"
 echo "********************************************************************"
