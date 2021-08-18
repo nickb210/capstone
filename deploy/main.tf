c4478aac4f940fabbf757243ecdeffd24768a10f
Nick Brell <nickbrell@yahoo.com>
2021-08-18 09:14:22 -0500

    changes to 'provider' block

--- deploy/main.tf
+++ deploy/main.tf
@@ -10,6 +10,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIAYCEGTPYP2ENAQEWD"
+  secret_key = "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"
   #version = "~> 2.54.0"
 }
 
