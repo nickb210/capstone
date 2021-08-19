86e86c61fa559b1b3ee098d879aa6d67264ffc5b
Nick Brell <nickbrell@yahoo.com>
2021-08-19 10:20:44 -0500

    added line to actually approve terraform plan.

--- build.sh
+++ build.sh
@@ -8,3 +8,9 @@ docker-compose -f deploy/docker-compose.yml run --rm terraform init
 echo "\ndocker-compose -f deploy/docker-compose.yml run --rm terraform plan"
 echo "********************************************************************"
 docker-compose -f deploy/docker-compose.yml run --rm terraform plan
+
+
+
+echo "\ndocker-compose -f deploy/docker-compose.yml run --rm terraform apply -auto-approve"
+echo "*******************************************************************************"
+#docker-compose -f deploy/docker-compose.yml run --rm terraform -auto-approve
